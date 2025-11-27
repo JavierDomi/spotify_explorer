a32cdbf387bb45b0069d3362dcb176cab5fd963a
JavierDomi <javier.dominguez.ib@gmail.com>
2025-11-27 09:07:45 +0100

    fix: standalone for ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,16 @@ module.exports = {
     apps: [
         {
             name: 'spotify_explorer',
-            script: 'node_modules/.bin/next',
-            args: 'start',
+            script: 'node',
+            args: '.next/standalone/server.js',
             cwd: '/var/www/spotify_explorer',
             instances: 1,
             autorestart: true,
             watch: false,
             max_memory_restart: '1G',
-            node_args: '-r dotenv/config',
             env_production: {
                 NODE_ENV: 'production',
                 PORT: 3000,
-                DOTENV_CONFIG_PATH: '/var/www/spotify_explorer/.env',
             },
         },
     ],
