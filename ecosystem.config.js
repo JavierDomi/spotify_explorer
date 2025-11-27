670b7f6356f816abb49fb31f1a2e5916abc94d6b
JavierDomi <javier.dominguez.ib@gmail.com>
2025-11-27 20:03:44 +0100

    git commit -m "feat: complete deploy workflow with rsync and env protection"

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,13 @@ module.exports = {
     apps: [
         {
             name: 'spotify_explorer',
-            script: 'pnpm',
-            args: 'start',
+            script: 'node',
+            args: '--env-file .env node_modules/.bin/next start',
             cwd: '/var/www/spotify_explorer',
             instances: 1,
             autorestart: true,
             watch: false,
             max_memory_restart: '1G',
-            env_file: '/var/www/spotify_explorer/.env',
             env_production: {
                 NODE_ENV: 'production',
                 PORT: 3000,
