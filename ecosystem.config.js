2158994edf221c1f1733451a53c666cf96105084
JavierDomi <javier.dominguez.ib@gmail.com>
2025-11-28 09:58:03 +0100

    fix: workflow for static files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'spotify_explorer',
-            script: 'node',
-            args: '.next/standalone/server.js',
+            script: './.next/standalone/server.js',
             cwd: '/var/www/spotify_explorer',
             instances: 1,
             autorestart: true,
@@ -13,6 +12,10 @@ module.exports = {
                 NODE_ENV: 'production',
                 PORT: 3000,
             },
+            error_file: './logs/err.log',
+            out_file: './logs/out.log',
+            log_file: './logs/combined.log',
+            time: true,
         },
     ],
 };
